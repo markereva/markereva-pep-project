cc81c3280e3edf2a155a939e13a9a20eee5358fa
Mark Elliott <mpegoku@gmail.com>
2024-12-07 11:52:34 +0000

    Implement AccountService

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,29 @@
+package Service;
+
+import DAO.AccountDAO;
+import Model.Account;
+
+public class AccountService {
+  private AccountDAO accountDAO;
+
+  public AccountService() {
+    accountDAO = new AccountDAO();
+  }
+
+  public AccountService(AccountDAO accountDAO) {
+    this.accountDAO = accountDAO;
+  }
+
+  public Account login(Account account) {
+    return accountDAO.getUserByUsernameAndPassword(account.getUsername(), account.getPassword());
+  }
+
+  public Account createAccount(Account account) {
+    int uLen = account.getUsername().length();
+    int pLen = account.getPassword().length();
+    boolean usernameInvalid = uLen < 4 || uLen > 255;
+    boolean passwordInvalid = pLen < 4 || pLen > 255;
+    if (usernameInvalid || passwordInvalid) return null;
+    return accountDAO.insertAccount(account);
+  }
+}
